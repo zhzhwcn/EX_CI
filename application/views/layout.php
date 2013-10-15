99609bf097d0b08322b0d29afa57bf9ec35df2f5
zhzhwcn <zhzhwmail@gmail.com>
2013-10-15 15:19:59 +0700

    添加引用JQUERY 修改框架引用文件

--- application/views/layout.php
+++ application/views/layout.php
@@ -5,7 +5,13 @@
 	<meta charset="utf-8">
 	<meta name="keywords" content="<?php echo $meta['keyword']?>" />
 	<meta name="description" content="<?php echo $meta['desc']?>" />
+	<script src="/static/js/jquery.js"></script>
 </head>
 <body>
+<?php
+	$this->load->view('common/header');
+	$this->load->view($this->uri->slash_segment(1).$this->uri->segment(2).'_layout');
+	$this->load->view('common/footer');
+?>
 </body>
 </html>
