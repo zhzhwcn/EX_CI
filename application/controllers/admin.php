0a0708f7497e5ce3823da861b48531f247f4566d
zhzhwcn <zhzhwmail@gmail.com>
2013-10-15 16:26:25 +0700

    增加后台需要的bootstrap引入文件

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -2,25 +2,52 @@
 
 class Admin extends EX_Controller {
 
-	/**
+	public function __construct()
+    {
+        parent::__construct();
+        $this->data['addon_header'][] = '<link rel="stylesheet" href="/static/bootstrap/css/bootstrap.css">';
+	    $this->data['addon_header'][] = '<link rel="stylesheet" href="/static/bootstrap/css/bootstrap-theme.css">';
+	    $this->data['addon_header'][] = '<script src="/static/bootstrap/js/bootstrap.js"></script>';
+    }
+    
+    /**
 	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/admin
-	 *	- or -  
-	 * 		http://example.com/index.php/admin/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/admin/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
 	{
 		// $this->load->view('welcome_message');
 	}
+    
+    /**
+     * Admin login
+     */
+    public function login()
+    {
+        
+    }
+    
+    /**
+     * Admin check login info
+     */
+    public function do_login()
+    {
+        
+    }
+    /**
+     * Admin logout 
+     */
+    public function logout()
+    {
+        
+    }
+    
+    /**
+     * Plugin list page
+     */
+    public function plugin_list()
+    {
+        
+    }
 }
 
 /* End of file admin.php */
