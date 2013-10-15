ade4613ee26c2c6f103131c1a70fbbbe80ba1423
zhzhwcn <zhzhwmail@gmail.com>
2013-10-15 13:12:59 +0700

    添加插件运行的基本函数

---
+++ application/helpers/plugin_helper.php
@@ -0,0 +1,30 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// ------------------------------------------------------------------------
+
+/**
+ * EX_CodeIgniter Plugin Helpers
+ *
+ * @package		EX_CodeIgniter
+ * @subpackage	Helpers
+ * @category	Helpers
+ * @author		zhzhwcn
+ */
+
+// ------------------------------------------------------------------------
+
+
+function plugin_add_url($url = '',$class = null,$function = '')
+{
+	
+}
+
+function plugin_add_hook($hook = '',$class = null,$order = 100)
+{
+	
+}
+
+function plugin_run_hook($hook = '',$argv = array())
+{
+	
+}
