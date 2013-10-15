f84308af09523716189cc3fbec1b5abd3466ebaa
zhzhwcn <zhzhwmail@gmail.com>
2013-10-15 15:26:56 +0700

    Admin Ctrler Init

---
+++ application/controllers/admin.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin extends EX_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/admin
+	 *	- or -  
+	 * 		http://example.com/index.php/admin/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/admin/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		// $this->load->view('welcome_message');
+	}
+}
+
+/* End of file admin.php */
+/* Location: ./application/controllers/admin.php */
