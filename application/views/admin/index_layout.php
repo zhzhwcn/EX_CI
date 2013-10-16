94661eabfdf12c3f71db47908670a5afc0f8aa74
zhzhwcn <zhzhwmail@gmail.com>
2013-10-16 15:54:47 +0700

    增加admin_index_layout

---
+++ application/views/admin/index_layout.php
@@ -0,0 +1,27 @@
+<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="#" class="navbar-brand">EX_CI</a>
+    </div>
+    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+      <ul class="nav navbar-nav">
+        <li class="active">
+          <a href="#">Login</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+
+<footer class="bs-footer" role="contentinfo">
+	<div class="container">
+		<p>Copyright &copy; Bravery 2012-<?php echo date('Y');?></p>
+	</div>
+</footer>
