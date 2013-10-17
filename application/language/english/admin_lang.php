1346e4140f10c807578671db9ed9a6660b0b8757
zhzhwcn <zhzhwmail@gmail.com>
2013-10-17 08:37:09 +0700

    增加admin语言文件

---
+++ application/language/english/admin_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['admin_no_user']				= 'Admin user not found.';
+$lang['admin_wrong_pass']			= 'Admin password is wrong.';
+/* End of file migration_lang.php */
+/* Location: ./application/language/english/admin_lang.php */
